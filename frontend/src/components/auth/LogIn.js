746df3c67e149e36ae5d0b05593e663f3905e876
Shane Schipper <shane99@vt.edu>
2020-07-26 15:58:47 -0400

    added basic login page

--- frontend/src/components/auth/LogIn.js
+++ frontend/src/components/auth/LogIn.js
@@ -1,29 +1,36 @@
 import React, { Component, Fragment } from 'react';
 import actions from '../../services/index'
+import '../../styles/LogIn.css'
 
 class LogIn extends Component {
 
     state = {
 
-    } 
-    handleChange = e => this.setState({[e.target.name]: e.target.value})
+    }
+    handleChange = e => this.setState({ [e.target.name]: e.target.value })
 
     handleSubmit = e => {
         e.preventDefault()
-         actions.logIn(this.state).then(user => {
-            this.props.setUser({...user.data})  
+        actions.logIn(this.state).then(user => {
+            this.props.setUser({ ...user.data })
         }).catch(({ response }) => console.error(response.data));
     }
     render() {
         return (
-            <Fragment>
+            <div id='login_div'>
                 <h2>LogIn</h2>
                 <form onSubmit={this.handleSubmit}>
-                    <input name="email" type="email" onChange={this.handleChange} />
-                    <input name="password" type="password" onChange={this.handleChange} />
-                    <input type="submit" value="Log In"/>
+                    <label for='email'>Email Address</label>
+                    <br />
+                    <input id='email' name="email" type="email" onChange={this.handleChange} />
+                    <br />
+                    <label for='Password'>Password</label>
+                    <br />
+                    <input id='password' name="password" type="password" onChange={this.handleChange} />
+                    <br />
+                    <input type="submit" value="Log In" />
                 </form>
-            </Fragment>
+            </div>
         );
     }
 }
