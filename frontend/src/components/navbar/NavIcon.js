3cc9a10b66167d57c3a28e986402907d17fc193b
Hanna Murai <annmuray75@gmail.com>
2020-07-28 15:17:36 -0500

    changes

--- frontend/src/components/navbar/NavIcon.js
+++ frontend/src/components/navbar/NavIcon.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import '../../styles/navbar_styles/NavIcon.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/navbar_styles/NavIcon.css";
 
-const NavIcon = (props) => {
+const NavIcon = props => {
+  return (
+    <Link to={props.destination}>
+      <img id="icon_img" src={props.icon} />
+    </Link>
+  );
+};
 
-    return (
-        <Link to={props.destination}>
-            <img id='icon_img' src={props.icon} />
-        </Link>
-    )
-}
-
-export default NavIcon
+export default NavIcon;
