838154e97657575c93b888d52f37ccb374af99f2
Hanna Murai <annmuray75@gmail.com>
2020-07-28 20:32:50 -0500

    nav-bar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+import "../styles/navbar_styles/Navbar.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div id="bar" className="navDone">
+      <Link to="/">
+        {" "}
+        <div id="logoNav" className="logoNav2">
+          <img src={require("../images/logo.png")} alt="logo" />
+          <h1>
+            <span>Job</span>Hunter
+          </h1>
+        </div>
+      </Link>
+      <nav role="navigation">
+        <div id="menuToggle">
+          <input type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+
+          <ul id="menu" className="navAll">
+            <Link to="/profile">
+              <div className="nav-links">
+                <img
+                  src={require("../images//userProfile.png")}
+                  alt="profile"
+                />
+                <p>Profile</p>
+              </div>
+            </Link>
+            <Link to="/jobs">
+              <div className="nav-links">
+                <img
+                  id="jobImg"
+                  src={require("../images/searchJob.png")}
+                  alt="searchJob"
+                />
+                <p>Jobs</p>
+              </div>
+            </Link>
+            <Link to="/companies">
+              <div className="nav-links">
+                <img src={require("../images/company.png")} alt="company" />
+                <p>Companies</p>
+              </div>
+            </Link>
+            <Link to="/post-job">
+              <div className="nav-links">
+                <img src={require("../images/postJob.png")} alt="postJob" />
+                <p>Post a job</p>
+              </div>
+            </Link>
+            <Link to="/contact">
+              <div className="nav-links">
+                <img
+                  id="contact"
+                  src={require("../images/contact.png")}
+                  alt="contact"
+                />
+                <p>Contact us</p>
+              </div>
+            </Link>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- frontend/src/components/auth/SignUp.js
+++ frontend/src/components/auth/SignUp.js
@@ -1,55 +1,85 @@
-import React, { Component, Fragment } from 'react';
-import actions from '../../services/index'
-import '../../styles/SignUp.css'
-import { Link } from 'react-router-dom'
-import logo from '../../images/logo.png'
-import Navbar from '../navbar/Navbar'
+import React, { Component, Fragment } from "react";
+import actions from "../../services/index";
+import "../../styles/SignUp.css";
+import { Link } from "react-router-dom";
+import logo from "../../images/logo.png";
+import Navbar from "../Navbar";
 
 class SignUp extends Component {
-    state = {
+  state = {};
 
-    }
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
-    handleChange = e => this.setState({ [e.target.name]: e.target.value })
+  handleSubmit = e => {
+    e.preventDefault();
+    actions
+      .signUp(this.state)
+      .then(user => {
+        this.props.setUser({ ...user.data });
+      })
+      .catch(({ response }) => console.error(response.data));
+  };
 
-    handleSubmit = e => {
-        e.preventDefault()
-        actions.signUp(this.state).then(user => {
-            this.props.setUser({ ...user.data })
-        }).catch(({ response }) => console.error(response.data));
-    }
-
-    render() {
-        return (
-            <div>
-            <Navbar />
-            <img src={logo} id='logo'/>
-                <div id='signup_div'>
-                    <h2>SignUP</h2>
-                    <form onSubmit={this.handleSubmit}>
-                        <label for='email'>Email Address</label>
-                        <br />
-                        <input id='email' name="email" type="email" onChange={this.handleChange} placeholder='example@example.com' className='input_field'/>
-                        <br />
-                        <label for='yourname'>Your Name</label>
-                        <br />
-                        <input id='yourname' name="name" type="text" onChange={this.handleChange} placeholder='John Smith' className='input_field'/>
-                        <br />
-                        <label for='jobtitle'>Job Title</label>
-                        <br />
-                        <input id='jobtitle' name="jobTitle" type="text" onChange={this.handleChange} placeholder='Web Developer' className='input_field'/>
-                        <br />
-                        <label for='password'>Password</label>
-                        <br />
-                        <input id='password' name="password" type="password" onChange={this.handleChange} placeholder='Password' className='input_field'/>
-                        <br />
-                        <input type="submit" value="Sign Up" id="signup_button" />
-                    </form>
-                    <Link to='/google-signup' id='suwg'>Sign Up With Google</Link>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div id="signup_div">
+          <h2>SignUP</h2>
+          <form onSubmit={this.handleSubmit}>
+            <label for="email">Email Address</label>
+            <br />
+            <input
+              id="email"
+              name="email"
+              type="email"
+              onChange={this.handleChange}
+              placeholder="example@example.com"
+              className="input_field"
+            />
+            <br />
+            <label for="yourname">Your Name</label>
+            <br />
+            <input
+              id="yourname"
+              name="name"
+              type="text"
+              onChange={this.handleChange}
+              placeholder="John Smith"
+              className="input_field"
+            />
+            <br />
+            <label for="jobtitle">Job Title</label>
+            <br />
+            <input
+              id="jobtitle"
+              name="jobTitle"
+              type="text"
+              onChange={this.handleChange}
+              placeholder="Web Developer"
+              className="input_field"
+            />
+            <br />
+            <label for="password">Password</label>
+            <br />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              onChange={this.handleChange}
+              placeholder="Password"
+              className="input_field"
+            />
+            <br />
+            <input type="submit" value="Sign Up" id="signup_button" />
+          </form>
+          <Link to="/google-signup" id="suwg">
+            Sign Up With Google
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SignUp;
