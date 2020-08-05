c22ae9c8b1947d2f2f42328b9811dc97cfa76fa0
Shane Schipper <shane99@vt.edu>
2020-08-05 14:55:10 -0400

    redeploying

--- backend/app.js
+++ backend/app.js
@@ -67,4 +67,8 @@ app.use("/", auth);
 // Uncomment this line for production
 let client = path.join(__dirname + "../public/index.html");
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+})
+
 module.exports = app;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -57,13 +57,6 @@ function isAuth(req, res, next) {
     : res.status(401).json({ msg: "Log in first" });
 }
 
-router.get('/*', (req, res) => {
-  console.log('everything', __dirname)
-  let url = path.join(__dirname, '../frontend/build', 'index.html');
-  // if (!url.startsWith('/app/')) // we're on local windows
-  //   url = url.substring(1);
-  res.sendFile(url);
-});
 
 module.exports = router;
 
