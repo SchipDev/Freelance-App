b5b80513ca796744c5eb754f5608fd4b978f3331
Hanna Murai <annmuray75@gmail.com>
2020-08-06 12:22:58 -0500

    profile

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -31,6 +31,7 @@ class Profile extends Component {
     showLoad: false,
     active1: true,
     active2: false,
+    active3: false,
     isEditingWE: false,
     isEditingSkills: false,
     isEditingEdu: false,
@@ -523,14 +524,23 @@ class Profile extends Component {
   };
   showRes = () => {
     this.setState({
-      active1: !this.state.active1,
-      active2: !this.state.active2
+      active1: true,
+      active2: false,
+      active3: false
     });
   };
   showCon = () => {
     this.setState({
-      active1: !this.state.active1,
-      active2: !this.state.active2
+      active1: false,
+      active2: true,
+      active3: false
+    });
+  };
+  showMycon = () => {
+    this.setState({
+      active1: false,
+      active2: false,
+      active3: true
     });
   };
   render() {
@@ -597,13 +607,19 @@ class Profile extends Component {
             className={this.state.active1 ? "active3" : null}
             onClick={this.showRes}
           >
-            Edit Resume
+            ✎ Edit Resume
           </span>{" "}
           <span
             onClick={this.showCon}
             className={this.state.active2 ? "active4" : null}
           >
-            Find new connections
+            ☏ Find new connections
+          </span>
+          <span
+            onClick={this.showMycon}
+            className={this.state.active3 ? "active5" : null}
+          >
+            ♖ See my connections
           </span>
         </p>
         {this.state.active1 ? (
@@ -621,6 +637,7 @@ class Profile extends Component {
           ""
         )}
         {this.state.active2 ? <UserSearch user={this.props.user.user} /> : ""}
+        {this.state.active3 ? <p>What`s up?</p> : ""}
       </div>
     );
   }
